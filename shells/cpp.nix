6141ecd93e526b805429d280528029eb7e8bf95c
Guntur Poetra <poetra.guntur@gmail.com>
2025-05-03 15:43:10 +0800

    feat: add more packages

--- shells/cpp.nix
+++ shells/cpp.nix
@@ -8,6 +8,7 @@
     nativeBuildInputs = with pkgs; [
       clang # C language family frontend for LLVM (wrapper script)
       cmake # Cross-platform, open-source build system generator
+      gcc # GNU Compiler Collection, version 14-20241116 (wrapper script)
       gnumake # Tool to control the generation of non-source files from sources
     ];
 
